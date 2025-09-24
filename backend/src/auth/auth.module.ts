1bf03800d9cba4877348a4a57fe17cc7841a3212
OmarElsayed66 <omarelsayed6600@gmail.com>
2025-09-24 18:08:17 +0300

    fix: security issue with reurned data

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserAuthModule } from './user-auth/user-auth.module';
+import { CustomerAuthModule } from './customer-auth/customer-auth.module';
 
 @Module({
-  imports: [UserAuthModule]
+  imports: [UserAuthModule, CustomerAuthModule]
 })
 export class AuthModule {}
